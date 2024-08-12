74c48ec8a148cf4a01a8329834448f16c202ab99
crcristian97 <crcristian97.cc@gmail.com>
2024-08-11 21:05:43 -0300

    configuracion vercel

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,23 +3,29 @@ from backend.generate_qr import generate_qr
 
 app = Flask(__name__)
 
-# Ruta para servir la página principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta para generar el código QR
 @app.route('/generate_qr', methods=['POST'])
 def generate_qr_code():
-    data = request.json
-    ssid = data.get('ssid')
-    password = data.get('password')
-    
-    if not ssid or not password:
-        return jsonify({"error": "SSID and Password are required"}), 400
-    
-    image_path = generate_qr(ssid, password)
-    return jsonify({"image_path": image_path}), 200
+    try:
+        data = request.json
+        ssid = data.get('ssid')
+        password = data.get('password')
+
+        if not ssid or not password:
+            return jsonify({"error": "SSID and Password are required"}), 400
+
+        image_path = generate_qr(ssid, password)
+        if not image_path:
+            return jsonify({"error": "Failed to generate QR code"}), 500
+
+        return jsonify({"image_path": image_path}), 200
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": "An internal error occurred"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
